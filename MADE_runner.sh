592790743628dbe12e4279310e9e05d6cf69cae5
Ahmad KhajeNezhad <khajenezhad@gpu-server1.gpu.ce.sharif.edu>
2018-05-23 07:24:48 +0430

    ahmads last changes after my mnist function insert

--- MADE_runner.sh
+++ MADE_runner.sh
@@ -2,18 +2,18 @@
 KERAS_BACKEND=tensorflow
 
 
-for HL in 'numhl2_hlsize70' 'numhl2_hlsize24'
+for HL in 'numhl2_hlsize80' 'numhl2_hlsize30'
 do
-	for i in 2 3 4 5
+	for i in 1 2 3 4 5
 	do
-		for ALG in 'orig' 'min_related' 'Q_restricted'
+		for ALG in 'orig' 'Q_restricted' #'min_related'
 		do
 			echo "$HL Running case #$i of 5 for method $ALG ..."
-			DEST_PATH="./results/grid_pos/$ALG/$HL/$i"
+			DEST_PATH="./results/Boltzmann/$ALG/$HL/$i"
 			TR=$((200*$i))
 			VAL=$((50*$i))
 			cp $DEST_PATH/config.py .
-			srun --gres=gpu:1 python3.5 main.py > "$DEST_PATH/results_grid_4by4_tr"$TR"_val"$VAL"_teFULL.txt" 2> cerr_log
+			srun --gres=gpu:1 python3.5 main.py > "$DEST_PATH/results_Boltzmann_10&10_tr"$TR"_val"$VAL"_teFULL.txt" 2> cerr_log
 		done
 	done
 done	
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ AE_adam = optimizers.Adam(lr=0.001, beta_1=0.1)
 num_of_exec = 10
 num_of_all_masks = 10
 num_of_hlayer = 2
-hlayer_size = 24
+hlayer_size = 30
 fit_iter = 1
 num_of_epochs = 2000   #max number of epoch if not reaches the ES condition
 batch_size = 50
@@ -18,16 +18,16 @@ train_size = 1000
 validation_size = 250
 test_size = 65536
 
-algorithm = 'Q_restricted'#'min_related'#'orig'
+algorithm = 'Q_restricted'#'orig'#'min_related'
 
-data_name = 'grid'
+data_name = 'Boltzmann'
 
 ## grid configs
 width = 4
 height = 4
+related_size = width
 
 ## mnist configs
-
 
 
 
@@ -40,12 +40,6 @@ m_boltzmann = 10
 if data_name == 'grid':
     graph_size = width * height
 elif data_name == 'mnist':
-    graph_size = 28*28 
+    graph_size = 28*28
 elif data_name == 'Boltzmann':
     graph_size = n_boltzmann + m_boltzmann
-
-##############
-if data_name == 'grid':
-    related_size = width
-elif data_name == 'mnist':
-    related_size = 28
