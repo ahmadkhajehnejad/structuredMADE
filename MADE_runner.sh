318bf9b74d61a8b014f300175c8b4e824683c0f3
Ahmad KhajeNezhad <khajenezhad@gpu-server1.gpu.ce.sharif.edu>
2018-08-15 00:53:31 +0430

    different number of masks

--- MADE_runner.sh
+++ MADE_runner.sh
@@ -2,18 +2,21 @@
 KERAS_BACKEND=tensorflow
 
 
-for HL in 'numhl2_hlsize80' 'numhl2_hlsize30'
+for HL in 'numhl2_hlsize100' 'numhl2_hlsize50' 'numhl2_hlsize25'  
 do
 	for i in 1 2 3 4 5
 	do
-		for ALG in 'orig' 'Q_restricted' #'min_related'
+		for ALG in 'ensemble_Q_restricted_and_orig' #'min_related' 'orig'
 		do
-			echo "$HL Running case #$i of 5 for method $ALG ..."
-			DEST_PATH="./results/Boltzmann/$ALG/$HL/$i"
-			TR=$((200*$i))
-			VAL=$((50*$i))
-			cp $DEST_PATH/config.py .
-			srun --gres=gpu:1 python3.5 main.py > "$DEST_PATH/results_Boltzmann_10&10_tr"$TR"_val"$VAL"_teFULL.txt" 2> cerr_log
+			for NM in 'num_masks_20'
+			do
+				echo "$HL Running case #$i of 5 for method $ALG ..."
+				DEST_PATH="./results/grid_new/$ALG/$NM/$HL/$i"
+				TR=$((200*$i))
+				VAL=$((50*$i))
+				cp $DEST_PATH/config.py .
+				srun --gres=gpu:1 python3.5 main.py > "$DEST_PATH/results_grid_4by4_tr"$TR"_val"$VAL"_teFULL.txt" 2> cerr_log
+			done
 		done
 	done
 done	
--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from keras import optimizers
 
 AE_adam = optimizers.Adam(lr=0.0003, beta_1=0.1)
 num_of_exec = 10
-num_of_all_masks = 10
+num_of_all_masks = 20
 num_of_hlayer = 2
-hlayer_size = 225
+hlayer_size = 100
 fit_iter = 1
 num_of_epochs = 2000   #max number of epoch if not reaches the ES condition
 batch_size = 50
@@ -16,11 +16,11 @@ patience = 20
 
 train_size = 200
 validation_size = 50
-test_size = 5000
+test_size = 'FULL_TEST'
 
-algorithm = 'Q_restricted'#'orig'#'min_related'#'ensemble_Q_restricted_and_orig'#
+algorithm = 'ensemble_Q_restricted_and_orig'#'Q_restricted'#'orig'#'Q_restricted'#'min_related'#
 
-data_name = 'rcv1'#'mnist'#'k_sparse'#'min_related'#'grid'#'mnist'#'Boltzmann'
+data_name = 'grid'#'mnist'#'Boltzmann'
 
 ## grid configs
 width = 4
@@ -35,10 +35,6 @@ digit = 6
 n_boltzmann = 10
 m_boltzmann = 10
 
-# k_sparse
-n_of_k_sparse = 20
-sparsity_degree = 3
-
 ##############
 
 if data_name == 'grid':
@@ -47,7 +43,3 @@ elif data_name == 'mnist':
     graph_size = 14*14
 elif data_name == 'Boltzmann':
     graph_size = n_boltzmann + m_boltzmann
-elif data_name == 'k_sparse':
-    graph_size = n_of_k_sparse
-elif data_name == 'rcv1':
-    graph_size = 150
