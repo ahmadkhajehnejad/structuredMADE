f58c9bc29792a32cf35e675be934127d5899e561
Ahmad KhajeNezhad <khajenezhad@gpu-server1.gpu.ce.sharif.edu>
2018-10-07 00:10:46 +0330

    new results format

--- collect_results.sh
+++ collect_results.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
-FILE_NAME_PREFIX='results_mnist_6'
+KERAS_BACKEND=tensorflow
+
+#####################################
+
+masks_no=10
+hlnum=2
+hlsize_list=(25 50 100 200 300)
+rnd_dim_ord=True # False # 'grid'
+direct_links=True # False # 'Full'
+algs_list=("orig" "Q_restricted") # 'min_related' # 'ensemble_Q_restricted_and_orig'
+dataset='grid' # 'k_sparse' # 'mnist' # 'Boltzmann' #
+rnd_data=False
+prefix='Average KLs: ' # 'avg Num of Connections ' # 'Variance KLs: ' # 
 
 
-echo ' '
-for HL in 'numhl2_hlsize300' 'numhl2_hlsize600'
-do
-	for prefix in 'Average NLLs: ' 'avg Num of Connections '
-	do
-		for ALG in 'orig' 'min_related' 'Q_restricted'
-		do
-			results="$ALG - $HL - $prefix ["
-			for i in 1 2 3 4
-			do
-				DEST_PATH="./$ALG/$HL/$i"
-				TR=$((200*$i))
-				VAL=$((50*$i))
-				TE=5000
-				catched_line=$(cat $DEST_PATH/$FILE_NAME_PREFIX"_tr"$TR"_val"$VAL"_te"$TE".txt" | grep "$prefix")
-				new_result=${catched_line#$prefix}
-				if [ $((i)) -lt 4 ]; then
-					results="$results $new_result,"
-				else
-					results="$results $new_result]"
-				fi
-			done
-			echo $results
+#####################################
+
+for hlsize in ${hlsize_list[@]}; do
+	for alg in ${algs_list[@]}; do
+		file_str_1="rnddata:$rnd_data - data:$dataset - alg:$alg - drlnk:$direct_links - rndord:$rnd_dim_ord - hlnum:$hlnum - hlsize:$hlsize - masksNum:$masks_no"
+		results="$file_str_1 - $prefix ["
+		for i in 1 2 3 4 5; do
+			TR=$((200*$i))
+			VAL=$((50*$i))
+			TE=5000
+			file_str="$file_str_1 - TR:$TR - VAL:$VAL - TE:$TE"
+			res_path="./results/result -- $file_str.txt"
+			catched_line=$(cat "$res_path" | grep "$prefix")
+			new_result=${catched_line#$prefix}
+			if [ $((i)) -lt 5 ]; then
+				results="$results $new_result,"
+			else
+				results="$results $new_result]"
+			fi
+
 		done
-		echo ' '
+		echo $results
 	done
 done
-#echo "============="
-#
-#for i in 1 2 3 4
-#do
-#	DEST_PATH="./results/$DATA_NAME/$ALG/$HL/$i"
-#	TR=$((200*$i))
-#	VAL=$((50*$i))
-#	cat $DEST_PATH/$FILE_NAME_PREFIX"_tr"$TR"_val"$VAL"_te"$TE".txt" | grep "Variance num of"
-#done
+
